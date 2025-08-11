c6575e2d109f4fb5897ba0387dee6d303e0f5e5f
Mandil Khadka <khadkamandil62@gmail.com>
2025-08-11 12:53:48 +0900

    done but refactoring and bootstrap left

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,5 +7,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @message = params[:answer]
+
+    if @message == "I am going to work"
+      @answer = "Great!"
+    elsif @message.end_with?("?")
+       @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
